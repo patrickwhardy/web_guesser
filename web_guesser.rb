27cd9129020a657e1291b34f998d4c9076360186
patrickwhardy <patrickwhardy@gmail.com>
2016-03-11 11:00:43 -0700

    color extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,14 +7,19 @@ get '/' do
   guess = params['guess'].to_i
   if guess > @@number + 5
     @@response = "Way Too High"
+    @@color = "crimson"
   elsif guess > @@number
     @@response = "Too High"
+    @@color = "coral"
   elsif guess < @@number - 5
     @@response = "Way Too Low"
+    @@color = "crimson"
   elsif guess < @@number
     @@response = "Too Low"
+    @@color = "coral"
   elsif guess == @@number
     @@response = "You got it right! The secret number is #{@@number}"
+    @@color = "aquamarine"
   end
   erb :index, :locals => {:number => @@number, :response => @@response}
 end
